d609eda038fd0869ee638bf66db27dac28b9001e
JIWEON-JEONG <dlektl6@naver.com>
2022-09-06 17:39:45 +0900

    [ADD] 직렬화

---
+++ PROJECT/TIL/src/main/java/STUDY/TIL/java/serialization/Default.java
@@ -0,0 +1,66 @@
+package STUDY.TIL.java.serialization;
+
+import java.io.*;
+import java.util.Date;
+import java.util.Calendar;
+
+/**
+ * 어디다 저장??? 파일 , 메모리,  DB 여러 방법이 있다.
+ *
+ * Role 1 : The object to be persisted 는 Serializable 을 구현하거나 구현된 객체를 상속받아야 한다.
+ *
+ */
+public class Default implements Serializable {
+
+    private Date time;
+
+    public Default()
+    {
+        time = Calendar.getInstance().getTime();
+    }
+
+    public Date getTime()
+    {
+        return time;
+    }
+
+    //FileInputStream : 파일의 내용을 읽음
+    //FileOutputStream : 파일의 내용을 작성함
+    public static void main(String[] args) {
+        String filename = "time.ser";
+        if (args.length > 0) {
+            filename = args[0];
+        }
+        Default time = new Default();
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(filename);
+            out = new ObjectOutputStream(fos);
+            //실제 직렬화 시작.
+            out.writeObject(time);
+            out.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+//----------------------------------------------
+
+        FileInputStream fis = null;
+        ObjectInputStream in = null;
+        try {
+            fis = new FileInputStream(filename);
+            in = new ObjectInputStream(fis);
+            //실제 직렬화 시작.
+            time = (Default) in.readObject();
+            in.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
---
+++ PROJECT/TIL/src/main/java/STUDY/TIL/java/serialization/Nonserializable.java
@@ -0,0 +1,34 @@
+package STUDY.TIL.java.serialization;
+
+import java.io.Serializable;
+
+/**
+ *반면에 Thread, OutputStream 및 해당 하위 클래스, Socket과 같은 특정 시스템 수준 클래스는 직렬화할 수 없습니다.
+ *
+ * transient : 직렬화 하는 과정에서 제외 하고 싶을 경우 사용.
+ *
+ * 패스워드와 같은 보안정보가 직렬화(Serialize) 과정에서 제외하고 싶은 경우에 적용합니다.
+ * 다양한 이유로 데이터를 전송을 하고 싶지 않을 때 선언할 수 있습니다.'
+ *
+ * Role #2: The object to be persisted must mark all nonserializable fields transient
+ */
+public class Nonserializable implements Serializable, Runnable {
+
+    transient private Thread animator;
+    private int animationSpeed;
+
+    public Nonserializable(int animationSpeed)
+    {
+        this.animationSpeed = animationSpeed;
+        animator = new Thread(this);
+        animator.start();
+    }
+
+    public void run()
+    {
+        while(true)
+        {
+            // do animation here
+        }
+    }
+}
