3c41f408e2ee5ba03859cb90e1739ae83143e09c
JIWEON-JEONG <dlektl6@naver.com>
2022-10-26 18:44:33 +0900

    [FEAT] Problem03 Solved

--- PROJECT/java-onboarding-main/src/main/java/onboarding/Problem3.java
+++ PROJECT/java-onboarding-main/src/main/java/onboarding/Problem3.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+//리팩토링 필요.
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        Problem3 p3 = new Problem3();
+        return p3.getTotalClap(number);
     }
+
+    public int getTotalClap(int number) {
+        int totalClap = 0;
+        for (int i = 1; i < number + 1; i++) {
+            totalClap += check369(i);
+        }
+        return totalClap;
+    }
+
+    public int check369(int num) {
+        int count = 0;
+        String value = String.valueOf(num);
+        for (char c : value.toCharArray()) {
+            if (c == '3' || c == '6' || c == '9') {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
